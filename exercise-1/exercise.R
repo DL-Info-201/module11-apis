c7afd0077e47d595123bfb41f6d51a283aa63b11
Dustin Langner <dustin.langner1@gmail.com>
2017-02-02 14:05:51 -0800

    complete exercise 1

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -8,15 +8,14 @@ library(jsonlite)
 ## Then send GET() request to fetch the data, then extract the answer to the question
 
 # For what years does the API have statistical data?
-  response <- GET("http://data.unhcr.org/api/stats/time_series_years.json")
-  content <- content(response, "text")
+  time <- GET("http://data.unhcr.org/api/stats/time_series_years.json")
+  content <- content(time, "text")
   json.content <- fromJSON(content)
-  json.content
-  
+
 # What is the "country code" for the "Syrian Arab Republic"?
   
-  response <- GET("http://data.unhcr.org/api/countries/list.json")
-  content <- content(response, "text")
+  countries <- GET("http://data.unhcr.org/api/countries/list.json")
+  content <- content(countries, "text")
   json <- fromJSON(content)
   country.code <- filter(json, name_en == "Syrian Arab Republic") %>% select(country_code)
   View(country.code)
@@ -25,26 +24,42 @@ library(jsonlite)
 # Hint: you'll need to use a query parameter
 # Use the `str()` function to print the data of interest
 # See http://www.unhcr.org/en-us/who-we-help.html for details on these terms
-
+  query.params <- list(country_of_residence = "USA", country_of_origin = "SYR", year = 2013)
+  response <- GET("http://data.unhcr.org/api/stats/persons_of_concern.json", query = query.params)
+  usa.persons <- fromJSON(content(response,"text"))
+  str(usa.persons)
 
 ## And this was only 2013...
 
 
 # How many *refugees* from Syria settled the USA in all years in the data set (2000 through 2013)?
 # Hint: check out the "time series" end points
+  
+  ref.params <- list(population_type_code = "RF", country_of_residence = "USA", country_of_origin = "SYR")
+  refugees <- GET("http://data.unhcr.org/api/stats/time_series_all_years.json", query=ref.params)
+  refugee.data <-fromJSON(content(refugees, "text"))
 
 
 # Use the `plot()` function to plot the year vs. the value.
 # Add `type="o"` as a parameter to draw a line
-
-
-
+ 
+   plot(refugee.data$year, refugee.data$value, type = "o")
+  
 # Pick one other country in the world (e.g., Turkey).
 # How many *refugees* from Syria settled in that country in all years in the data set (2000 through 2013)?
 # Is it more or less than the USA? (Hint: join the tables and add a new column!)
 # Hint: To compare the values, you'll need to convert the data (which is a string) to a number; try using `as.numeric()`
 
+   query.params <- list(country_of_residence = "TUR", country_of_origin = "SYR", population_type_code = "RF")
+   response <- GET("http://data.unhcr.org/api/stats/time_series_all_years.json?", query = query.params)
+   refugees.other <- fromJSON(content(response,"text"))
+   refugees.other <- select(refugees.other, year, other = value)
+   refugees.other
 
+   total.data <- left_join(refugees.data, refugees.other, by="year") %>%
+     mutate(usa = as.numeric(usa), other = as.numeric(other)) %>%
+     mutate(usa.more = usa > other)
+   View(total.data)
 
 ## Bonus (not in solution):
 # How many of the refugees in 2013 were children (between 0 and 4 years old)?
